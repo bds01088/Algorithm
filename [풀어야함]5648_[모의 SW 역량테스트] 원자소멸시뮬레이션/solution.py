9210257c82272be877f782c1ad52249528c62321
bds01088 <bds01088@naver.com>
2022-04-09 23:04:17 +0900

    풀어야함

--- [풀어야함]5648_[모의 SW 역량테스트] 원자소멸시뮬레이션/solution.py
+++ [풀어야함]5648_[모의 SW 역량테스트] 원자소멸시뮬레이션/solution.py
@@ -10,57 +10,90 @@
 9. 원자들은 이동 방향은 처음에 주어진 방향에서 바뀌지 않는다.
 10. 원자들이 충돌하여 소멸되며 방출되는 에너지는 다른 원자의 위치나 이동 방향에 영향을 주지 않는다.
 
-해결방법
-원자의 좌표범위에 대한 제한이 없기 때문에 무한하다 생각된다
-그렇다면 해당 좌표를 받고
-또다른 리스트를만들어서 이동방향을 담아둔 뒤
-while문으로 이동방향으로 계속 이동하면서 좌표자체가 같은 곳이 있다면 충돌한 것으로
-다만 충돌할때의 원자 방향들이 일직선상에 있다면 .5의 길이도 존재하므로 고려해야 한다
-또한 여러 원자들이 부딧히면 다같이 없어져야 하며
-while문의 종료 시점을 원자가 1개만 남거나,
-남은 원자들의 이동방향과 좌표의 형태가 가능성이 1도 없다는 것을 알았을때 종료시켜야한다
-
 가능성이 없는 종류
 방향도 고려해야함
-위로 이동하는 원자일때 자신보다 row값이 낮은게 없을 경우 가능성x
-아래로 이동하는 원자일 때 자신보다 row값이 높은게 없을 경우 가능성x
-좌로 이동하는 원자일 때 자신보다 col값이 낮은 게 없을 경우 가능성x
-우로 이동하는 원자일 때 자신보다 col값이 높은게 없을 경우 가능성x
-
-수직일때
-a가 가로로 이동하고 b가 세로로 이동할 떄
-a의 방향이 b의 col쪽으로 줄어들고
-b의 방향이 a의 row쪽으로 줄어들면서
-a의 col이동거리가 b의 row이동거리와 동일하다면 충돌함
-
+위로 이동하는 원자일때 자신보다 y값이 낮은게 없을 경우 가능성x
+아래로 이동하는 원자일 때 자신보다 y값이 높은게 없을 경우 가능성x
+좌로 이동하는 원자일 때 자신보다 x값이 낮은 게 없을 경우 가능성x
+우로 이동하는 원자일 때 자신보다 x값이 높은게 없을 경우 가능성x
 
 0.5초에 충돌하는건 어떻게하지?
-진행방향으로 한칸 옮긴 좌표가 일직선상에서 타 원자의 좌표값과 동일하다면 가능
-'''
+좌표를 -1000 이상 1000이하의 정수로 주어지는걸
+*2해서 0.5초를 1초로 바꾸어 계산하자
+즉 좌표는 -2000~2000이 되는거다
 
-def find_
+해결방법
+모든 원자들의 좌표와 이동방향을 저장하고
+0.5초 마다 한칸씩 이동시킨다
+이동 후 원자들의 좌표 목록에서
+동일한 좌표가 있다면 그 좌표에 있는 모든 원자들을 삭제함과 동시에
+들고있는 에너지들을 결과에 더해주자
+'''
+import sys
+sys.stdin = open('input.txt')
 
 tc = int(input())
+
+#상 하 좌 우
+#0  1  2  3
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
 
 for t in range(tc):
     n = int(input())
     atom = []
-    atom_d = []
-    atom_e = []
+    crash = [0 for _ in range(n)]
     for i in range(n) :
         row, col, direction, energy = map(int, input().split())
-        atom.append([row,col])
-        atom_d.append(direction)
-        atom_e.append(energy)
+        atom.append([row*2,col*2, direction, energy])
+        
+    ans = 0
+    for _ in range(4002):
+        del_list = []
+        v = []
+        trash_list = []
+        #시작좌표가 동일한 적은 없다 했으니 이동부터 하자
+        for i in range(len(atom)):
+            atom[i][0] += dx[atom[i][2]]
+            atom[i][1] += dy[atom[i][2]]
+            if abs(atom[i][0]) > 2000 or abs(atom[i][1]) > 2000 :
+                trash_list.append([atom[i][0],atom[i][1]])
 
-    #상 하 좌 우
-    #0  1  2 3
-    dr = [-1, 1, 0, 0]
-    dc = [0, 0, -1, 1]
-    flag = 0
-    time = 0
-    while flag == 0:
-        for i in range(n):
+        for i in range(len(atom)):
+            x, y = atom[i][0], atom[i][1]
+            if [x,y] in v:
+                del_list.append([x,y])
+            else :
+                v.append([x,y])
 
+        for i in range(len(atom)):
+            prob = 0
+            for j in range(len(atom)):
+                #위로 이동하는데 y값이 더 큰게 없다면 가망성x
+                if atom[i][2] == 0 and atom[i][1] < atom[j][1]:
+                    prob += 1
+                #아래로 이동할때 y값이 더 작은게 없다면 가망성x
+                elif atom[i][2] == 1 and atom[i][1] > atom[j][1] :
+                    prob += 1
+                #왼쪽으로 이동할때 x값이 더 작은게 없으면 가망성x
+                elif atom[i][2] == 2 and atom[i][0] > atom[j][0] :
+                    prob += 1
+                elif atom[i][2] == 3 and atom[i][0] < atom[j][0] :
+                    prob += 1
+            if prob == 0:
+                trash_list.append([atom[i][0],atom[i][1]])
+        if del_list:
+            for i in range(len(atom)-1, -1, -1):
+                if [atom[i][0], atom[i][1]] in del_list:
+                    ans += atom[i][3]
+                    atom.pop(i)
 
+        if trash_list :
+            for i in range(len(atom)-1, -1, -1):
+                if [atom[i][0], atom[i][1]] in trash_list:
+                    atom.pop(i)
+        
+        if len(atom) < 2:
+            break
 
+    print(f'#{t+1} {ans}')
