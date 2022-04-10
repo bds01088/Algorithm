0d883865a7fc32805477057f8f6402144cbf5535
bds01088 <bds01088@naver.com>
2022-04-10 17:51:15 +0900

    여전히 문제있음 해결해야됌

--- [풀어야함]5648_[모의 SW 역량테스트] 원자소멸시뮬레이션/solution.py
+++ [풀어야함]5648_[모의 SW 역량테스트] 원자소멸시뮬레이션/solution.py
@@ -32,6 +32,35 @@
 import sys
 sys.stdin = open('input.txt')
 
+def find(startnode):
+    global crash, ans, atom, board
+    node = startnode
+    while crash[node] != 1:
+        #시작원자에서 가장 작은 거리를 가진 원자번호 구하기
+        idx = board[node].index(min(board[node]))
+        #만약 그 원자가 안사라져있고, 거기서 최솟값이 시작원자와 같다면
+        #시작원자 A와 가장 작은 거리를 가진 원자 B가 제일 먼저 부딧힘
+        if crash[idx] == 0 and min(board[idx]) == min(board[node]) :
+            #B와의 강능성이 있는 노드들 중 동시에 부딧히는 원자가 더 있는지 확인하고
+            for j in range(n):
+                #있다면 답에 에너지 넣어주고 충돌했다고 상태를 변경해줌
+                if board[idx][j] == min(board[idx]):
+                    #최소한 A원자는 값이 더해짐
+                    ans += atom[j][3]
+                    crash[j] = 1
+            #마지막으로 B의 에너지를 넣어줌
+            ans += atom[idx][3]
+            crash[idx] = 1
+        #시작원자 A의 최소거리와 A의 최소거리에 해당하는 원자 B의 최소거리가 다르다면
+        #B는 다른 원자 C와 먼저 부딧혀 소멸할 것이다
+        #그러나 C가 먼저 따른원자 D와 부딧혀 소멸하면, B의 최소거리 원자는 달라질 것이다
+        #이걸 어떻게 해결하지?
+        else :
+            x = min(board[idx])
+            newstartnode = board[idx].index(x)
+            find(newstartnode)
+        
+
 tc = int(input())
 
 #상 하 좌 우
@@ -41,59 +70,59 @@ dy = [1, -1, 0, 0]
 
 for t in range(tc):
     n = int(input())
+    ans = 0
     atom = []
+    board = [[9999999]*n for _ in range(n)]
     crash = [0 for _ in range(n)]
     for i in range(n) :
         row, col, direction, energy = map(int, input().split())
         atom.append([row*2,col*2, direction, energy])
-        
-    ans = 0
-    for _ in range(4002):
-        del_list = []
-        v = []
-        trash_list = []
-        #시작좌표가 동일한 적은 없다 했으니 이동부터 하자
-        for i in range(len(atom)):
-            atom[i][0] += dx[atom[i][2]]
-            atom[i][1] += dy[atom[i][2]]
-            if abs(atom[i][0]) > 2000 or abs(atom[i][1]) > 2000 :
-                trash_list.append([atom[i][0],atom[i][1]])
+    
+    #가망성 있는 원자와의 거리 구하기
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == 9999999:
+                #기준원자의 방향이 위로 갈때
+                if atom[i][2] == 0 :
+                    #비교원자의 방향이 아래일때
+                    if atom[j][2] == 1 and atom[i][0] == atom[j][0] :
+                        c = atom[j][1] - atom[i][1]
+                        board[i][j] = c
+                        board[j][i] = c
+                    #오른쪽에서 왼쪽으로 올때
+                    elif atom[j][2] == 2 and atom[j][0]-atom[i][0] > 0 and atom[j][1]-atom[i][1] > 0 and atom[j][0]-atom[i][0] == atom[j][1]-atom[i][1] :
+                        c = (atom[j][0]-atom[i][0]) + (atom[j][1]-atom[i][1])
+                        board[i][j] = c
+                        board[j][i] = c
+                    #왼쪽에서 오른쪽으로 올때
+                    elif atom[j][2] == 3 and atom[i][0] - atom[j][0] > 0 and atom[j][1]-atom[i][1] > 0 and atom[i][0]-atom[j][0] == atom[j][1]-atom[i][1] :
+                        c = (atom[i][0]-atom[j][0]) + (atom[j][1]-atom[i][1])
+                        board[i][j] = c
+                        board[j][i] = c
+                #기준원자의 방향이 왼쪽으로 갈때
+                elif atom[i][2] == 2 :
+                    #비교원자의 방향이 오른쪽으로 움직일때
+                    if atom[j][2] == 3 and atom[i][1] == atom[j][1] :
+                        c = atom[i][0] - atom[j][0]
+                        board[i][j] = c
+                        board[j][i] =c
+                    #위에서 아래로 올때
+                    elif atom[j][2] == 1 and atom[i][0]-atom[j][0] > 0 and atom[j][1]-atom[i][1] > 0 and atom[i][0]-atom[j][0] == atom[j][1]-atom[i][1]:
+                        c = (atom[i][0]-atom[j][0]) + (atom[j][1]-atom[i][1])
+                        board[i][j] = c
+                        board[j][i] = c
+                #기준원자가 위에서 아래로 내려올때
+                elif atom[i][2] == 1 :
+                    #비교원자가 왼쪽에서 오른쪽으로 올때
+                    if atom[j][2] == 3 and atom[i][0]-atom[j][0] > 0 and atom[i][1]-atom[j][1] > 0 and atom[i][0]-atom[j][0] == atom[i][1]-atom[j][1]:
+                        c = (atom[i][0]-atom[j][0]) + (atom[i][1]-atom[j][1])
+                        board[i][j] = c
+                        board[j][i] = c
 
-        for i in range(len(atom)):
-            x, y = atom[i][0], atom[i][1]
-            if [x,y] in v:
-                del_list.append([x,y])
-            else :
-                v.append([x,y])
-
-        for i in range(len(atom)):
-            prob = 0
-            for j in range(len(atom)):
-                #위로 이동하는데 y값이 더 큰게 없다면 가망성x
-                if atom[i][2] == 0 and atom[i][1] < atom[j][1]:
-                    prob += 1
-                #아래로 이동할때 y값이 더 작은게 없다면 가망성x
-                elif atom[i][2] == 1 and atom[i][1] > atom[j][1] :
-                    prob += 1
-                #왼쪽으로 이동할때 x값이 더 작은게 없으면 가망성x
-                elif atom[i][2] == 2 and atom[i][0] > atom[j][0] :
-                    prob += 1
-                elif atom[i][2] == 3 and atom[i][0] < atom[j][0] :
-                    prob += 1
-            if prob == 0:
-                trash_list.append([atom[i][0],atom[i][1]])
-        if del_list:
-            for i in range(len(atom)-1, -1, -1):
-                if [atom[i][0], atom[i][1]] in del_list:
-                    ans += atom[i][3]
-                    atom.pop(i)
-
-        if trash_list :
-            for i in range(len(atom)-1, -1, -1):
-                if [atom[i][0], atom[i][1]] in trash_list:
-                    atom.pop(i)
-        
-        if len(atom) < 2:
-            break
-
+    for i in range(n):
+        if min(board[i]) == 9999999:
+            crash[i] = 1
+            continue
+        if crash[i] == 0:
+            find(i)
     print(f'#{t+1} {ans}')
